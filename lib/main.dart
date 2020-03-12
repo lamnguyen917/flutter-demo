4ae183a756ad530d9e4ef69c83a5481a90e0eb15
LamNX <lamnx@rikkeisoft.com>
2020-03-12 15:12:48 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Text('Flutter Demo Home Page'),
+    );
+  }
+}
